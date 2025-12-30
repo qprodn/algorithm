37341b9ce9d609adb02fd01c384e8fb5488cb736
QiNian <g2864229430@gmail.com>
2025-12-30 20:38:17 +0800

    20251230

---
+++ linkedlist/移除链表元素203_test.go
@@ -0,0 +1,22 @@
+package linkedlist
+
+import "testing"
+
+func removeElements(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{
+		Next: head,
+	}
+	cur := dummyHead
+	for cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummyHead.Next
+}
+
+func TestName(t *testing.T) {
+
+}
