891caae4076a1d85d13d00ce5211a73b2d152bfe
guiqinian <guiqinian@Shanzhen.com>
2026-01-16 18:00:18 +0800

    层序遍历

---
+++ binarytree/二叉树的层序遍历102_test.go
@@ -0,0 +1,28 @@
+package binarytree
+
+func levelOrder(root *TreeNode) [][]int {
+	count := 1
+	ans := make([][]int, 0)
+	if root == nil {
+		return ans
+	}
+	list := make([]*TreeNode, 0)
+	list = append(list, root)
+	for len(list) > 0 {
+		c := make([]int, 0)
+		for i := 0; i < count; i++ {
+			node := list[0]
+			list = list[1:]
+			if node.Left != nil {
+				list = append(list, node.Left)
+			}
+			if node.Right != nil {
+				list = append(list, node.Right)
+			}
+			c = append(c, node.Val)
+		}
+		count = len(list)
+		ans = append(ans, c)
+	}
+	return ans
+}
